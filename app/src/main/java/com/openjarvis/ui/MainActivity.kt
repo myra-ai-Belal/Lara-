ef0ddf585484bd4d02d089079a0549242346ded9
myra-ai-Belal <myra59538@gmail.com>
2026-09-07 03:53:42 +0600

    Update MainActivity.kt

--- app/src/main/java/com/openjarvis/ui/MainActivity.kt
+++ app/src/main/java/com/openjarvis/ui/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
